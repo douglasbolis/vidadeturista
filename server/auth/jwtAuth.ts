a6155bcb5c250a046b3e7e9bdfa4f230022b6717
douglasbolis <douglas.lima@xdevel.com.br>
2018-06-06 11:35:10 -0300

    feat(passport): Modificado classe do passport.

--- server/auth/jwtAuth.ts
+++ server/auth/jwtAuth.ts
@@ -1,5 +1,22 @@
-import { AppConfig } from '../config/app-config'
+import { AppConfig } from '../config'
 
-export const authenticate: Function = ( passport: any, appConfig: AppConfig ) => {
-  return passport.authenticate( appConfig.getJwtConfig().strategy, appConfig.getJwtConfig().session )
+/**
+ *Classe para a autenticação do usuário.
+ *
+ * @export
+ * @class Auth
+ */
+export class Auth {
+  /**
+   * Método de verificação da autenticidade do usuário.
+   *
+   * @static
+   * @param {*} passport Definição do passport.
+   * @param {AppConfig} appConfig Objeto com os dados para validação do token no header da requisição.
+   * @returns {*}
+   * @memberof Auth
+   */
+  public static authenticate ( passport: any, appConfig: AppConfig ): any {
+    return passport.authenticate( appConfig.getJwtConfig().strategy, appConfig.getJwtConfig().session )
+  }
 }
